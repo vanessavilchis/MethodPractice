9ee015ae245bc617ff428a95b8b9bc7913ee1139
vanessavilchis <vanessavilchis.info@gmail.com>
2025-09-25 13:18:01 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Menu {
+    public static void main(String[] args) {
+        displayMenu();
+    }
+
+    private static void displayMenu() {
+        System.out.println("MENU \n1. Coffee - 3.99 \n2. Tea - $2.99 \n3. Cookie - $1.99");
+    }
+
+}
